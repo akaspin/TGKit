9e21ef43bde17a81cf40f291202596bc47454174
Paul Eipper <paul@nkey.com.br>
2014-10-21 17:58:58 -0200

    Initial Commit

---
+++ TGKit/TGKit.h
@@ -0,0 +1,13 @@
+//
+//  TGKit.h
+//  TGKit
+//
+//  Created by Paul Eipper on 21/10/2014.
+//  Copyright (c) 2014 nKey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGKit : NSObject
+
+@end
---
+++ TGKit/TGKit.m
@@ -0,0 +1,13 @@
+//
+//  TGKit.m
+//  TGKit
+//
+//  Created by Paul Eipper on 21/10/2014.
+//  Copyright (c) 2014 nKey. All rights reserved.
+//
+
+#import "TGKit.h"
+
+@implementation TGKit
+
+@end
