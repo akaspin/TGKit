3a424fd0abbcc9f7f4ecbf8a4a9f4bfc1fec4ef3
Paul Eipper <paul@nkey.com.br>
2014-10-28 14:24:10 -0200

    Move loop source to libtgl and fix license

--- libtgl/loop.h
+++ libtgl/loop.h
@@ -1,10 +1,22 @@
-//
-//  loop.h
-//  TGKit
-//
-//  Created by Paul Eipper on 24/10/2014.
-//  Copyright (c) 2014 nKey. All rights reserved.
-//
+/*
+ This file is part of telegram-cli.
+ 
+ Telegram-cli is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ Telegram-cli is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this telegram-cli.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ Copyright Vitaly Valtman 2013-2014
+ Copyright Paul Eipper 2014
+ */
 
 #ifndef __TGKit__loop__
 #define __TGKit__loop__
