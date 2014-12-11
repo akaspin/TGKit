444d7811264dfe1d9e35f775a658c3fa3ef17ecd
Paul Eipper <paul@nkey.com.br>
2014-12-11 16:37:34 -0200

    Initial TGNet files

---
+++ TGKit/tgnet-c.h
@@ -0,0 +1,13 @@
+//
+//  tgnet-c.h
+//  TGKit
+//
+//  Created by Paul Eipper on 8/12/2014.
+//  Copyright (c) 2014 nKey. All rights reserved.
+//
+
+#ifndef TGKit_tgnet_c_h
+#define TGKit_tgnet_c_h
+
+
+#endif
